2bf427a8047a9ea065a10226c329c3b3a444ed1d
apexinite <katanacut@gmail.com>
2014-12-06 16:54:13 +0800

    new updates - level 2 - updated physics - added button and other shit

--- main.lua
+++ main.lua
@@ -10,9 +10,35 @@ local physics = require ("physics")
 physics.start()
 
 local LD = require ("lib.LD_LoaderG2")
- 
 local myLevel = {}
+-- local btnChangeLvl = nil
+-- local btnPressed = false
+
+-- local function btnReleased(event)
+-- 	player:setSequence("up")
+-- 	player:play()
+
+-- 	btnPressed = false
+-- end
+
+-- local function nextLvl(event)
+-- 	if (isActive) then
+-- 		player
+-- 	end
+-- end
 
 -- this creates all the display objects for the level
 -- myLevel will contain all the display groups so you can access all the elements of the level
 myLevel = LD:loadLevel("level1")
+
+local function nextLvl(event)
+	if (isActive) then
+		-- player.setSequence("next")
+		-- player:play()
+
+		-- btnPressed = true
+		myLevel = LD:loadLevel("Level2")
+	end
+end
+
+btnChangeLvl.onPress = nextLvl
